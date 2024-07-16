45d23cfd9aef1153e734c50bba8af9cfd2ab715e
nexgrid <trsoo24@nexgrid.co.kr>
2024-07-16 16:46:28 +0900

    ✨ feat : Pet Repository 조회 Method

---
+++ src/main/java/com/spring/mummus/service/reference/PetReference.java
@@ -0,0 +1,20 @@
+package com.spring.mummus.service.reference;
+
+import com.spring.mummus.exception.exception.PetException;
+import com.spring.mummus.repository.PetRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import static com.spring.mummus.exception.enums.PetErrorCode.DUPLICATED_PET;
+
+@Service
+@RequiredArgsConstructor
+public class PetReference {
+    private final PetRepository petRepository;
+
+    public void checkDuplicatedPet(String petName, Long memberId) {
+        if (petRepository.existsPet(petName, memberId)) {
+            throw new PetException(DUPLICATED_PET);
+        }
+    }
+}
