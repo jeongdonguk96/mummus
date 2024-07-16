53e13c18a2218913ab224f5b21cdf4e1a97121db
JeongDongUk <foodsmith96@naver.com>
2024-07-16 13:04:30 +0900

    feat: 프로젝트 생성

---
+++ src/main/java/com/spring/mummus/MummusApplication.java
@@ -0,0 +1,13 @@
+package com.spring.mummus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MummusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MummusApplication.class, args);
+	}
+
+}
