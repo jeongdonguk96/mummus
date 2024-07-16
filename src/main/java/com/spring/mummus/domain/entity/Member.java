2d0e71149764c79add8e2945f71fa5c00643b8e0
nexgrid <trsoo24@nexgrid.co.kr>
2024-07-16 16:40:25 +0900

    ✨ feat : Member Entity 생성

---
+++ src/main/java/com/spring/mummus/domain/entity/Member.java
@@ -0,0 +1,23 @@
+package com.spring.mummus.domain.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.*;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+@Builder
+public class Member {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String email;
+    private String password;
+    private String name;
+    private String address;
+    private String phoneNumber;
+}
