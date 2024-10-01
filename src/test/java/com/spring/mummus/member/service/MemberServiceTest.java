f6751284410ce39bd0de3dca827766ca0402875b
jungdonguk <foodsmith96@naver.com>
2024-10-01 18:00:53 +0900

    ✅ Test: MemberService 테스트 코드 작성 완료

--- src/test/java/com/spring/mummus/member/service/MemberServiceTest.java
+++ src/test/java/com/spring/mummus/member/service/MemberServiceTest.java
@@ -1,5 +1,6 @@
 package com.spring.mummus.member.service;
 
+import com.spring.mummus.exception.exception.MemberException;
 import com.spring.mummus.member.dto.MemberSignUpRequest;
 import com.spring.mummus.member.entity.Member;
 import com.spring.mummus.member.repository.MemberRepository;
@@ -8,11 +9,17 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import static org.assertj.core.api.Assertions.*;
+
+import static com.spring.mummus.exception.enums.MemberErrorCode.DUPLICATED_EMAIL;
+import static com.spring.mummus.exception.enums.MemberErrorCode.DUPLICATED_PHONE_NUMBER;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 
 @SpringBootTest
 class MemberServiceTest {
+
     @Autowired MemberService memberService;
     @Autowired MemberRepository memberRepository;
 
@@ -25,7 +32,7 @@ class MemberServiceTest {
   
     @Test
     @DisplayName("회원가입이 성공한다.")
-    void signUpSuccess() {
+    void signUpTest() {
         //given
         MemberSignUpRequest memberSignUpRequest = new MemberSignUpRequest("test@naver.com", "password", "testName", "testAddress", "testPhoneNumber");
 
@@ -40,5 +47,57 @@ class MemberServiceTest {
         assertThat(member.getAddress()).isEqualTo("testAddress");
         assertThat(member.getPhoneNumber()).isEqualTo("testPhoneNumber");
     }
+
+
+    @Test
+    @DisplayName("중복된 이메일을 확인한다.")
+    void checkDuplicatedEmailTest() {
+        //given
+        MemberSignUpRequest memberSignUpRequest1 = new MemberSignUpRequest("test@naver.com", "password1", "testName1", "testAddress1", "testPhoneNumber1");
+        MemberSignUpRequest memberSignUpRequest2 = new MemberSignUpRequest("test@naver.com", "password2", "testName2", "testAddress2", "testPhoneNumber2");
+
+        //when
+        memberService.signUp(memberSignUpRequest1);
+        MemberException memberException = assertThrows(MemberException.class, () -> {
+            memberService.signUp(memberSignUpRequest2);
+        });
+
+        //then
+        assertEquals(DUPLICATED_EMAIL, memberException.getErrorCode());
+    }
+
+
+    @Test
+    @DisplayName("중복된 전화번호를 확인한다.")
+    void checkDuplicatedPhoneNumberTest() {
+        //given
+        MemberSignUpRequest memberSignUpRequest1 = new MemberSignUpRequest("test@naver.com1", "password1", "testName1", "testAddress1", "testPhoneNumber");
+        MemberSignUpRequest memberSignUpRequest2 = new MemberSignUpRequest("test@naver.com2", "password2", "testName2", "testAddress2", "testPhoneNumber");
+
+        //when
+        memberService.signUp(memberSignUpRequest1);
+        MemberException memberException = assertThrows(MemberException.class, () -> {
+            memberService.signUp(memberSignUpRequest2);
+        });
+
+        //then
+        assertEquals(DUPLICATED_PHONE_NUMBER, memberException.getErrorCode());
+    }
+
+
+    @Test
+    @DisplayName("회원 존재 여부가 확인된다.")
+    void findByIdTest() {
+        //given
+        MemberSignUpRequest memberSignUpRequest1 = new MemberSignUpRequest("test@naver.com1", "password1", "testName1", "testAddress1", "testPhoneNumber");
+
+        //when
+        Member member = memberRepository.findById(1L).get();
+        Member targetMember = memberService.findById(member.getId());
+
+        //then
+        assertThat(member).isEqualTo(targetMember);
+    }
+
 
 }
