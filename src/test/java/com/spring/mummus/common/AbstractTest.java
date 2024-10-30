73d08ac8632dcd0bde8ec517f53b5c979e922dc3
jungdonguk <foodsmith96@naver.com>
2024-10-30 19:32:26 +0900

    ✨ Feat: 테스트 환경에서 S3 대신 localstack 연동

--- src/test/java/com/spring/mummus/common/AbstractTest.java
+++ src/test/java/com/spring/mummus/common/AbstractTest.java
@@ -17,12 +17,14 @@ import org.springframework.beans.factory.InitializingBean;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 import java.util.stream.Collectors;
 
 @Transactional
+@ActiveProfiles("test")
 @SpringBootTest
 public abstract class AbstractTest implements InitializingBean {
 
